ca51c6f659413d0b90638c7b9063946b7e2f6755
travisblair74 <travisblair74@gmail.com>
2018-01-17 11:19:49 -0500

    Added unique post and comment

--- bloccit/db/seeds.rb
+++ bloccit/db/seeds.rb
@@ -1,26 +1,25 @@
  require 'random_data'
 
- # Create Posts
+
  50.times do
- # #1
+
    Post.create!(
- # #2
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
  end
  posts = Post.all
  
- # Create Comments
- # #3
  100.times do
    Comment.create!(
- # #4
      post: posts.sample,
      body: RandomData.random_paragraph
    )
  end
  
+ Post.find_or_create_by(title: "I'm right", body: "I'm right and you're wrong I know it.")
+ Comment.find_or_create_by(body: "Says who?")
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
